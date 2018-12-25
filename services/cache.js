dedbc97c8e06e61c88e74bbe5349dcdd9d92e76f
bbudron <bbudron@gmail.com>
2018-12-25 12:45:35 -0500

    Fixing problems with redis for prod

--- services/cache.js
+++ services/cache.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
+const util = require('util');
 
 if (process.env.REDISTOGO_URL) {
     const redisToGo = require("url").parse(process.env.REDISTOGO_URL);
     let client = redis.createClient(redisToGo.port, redisToGo.hostname);
     client.auth(redisToGo.auth.split(":")[1]);
-
+    client.hget = util.promisify(client.hget);
 
 } else {
     let client = redis.createClient('redis://127.0.0.1:6379');
+    client.hget = util.promisify(client.hget);
 }
 
-const util = require('util');
-client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
