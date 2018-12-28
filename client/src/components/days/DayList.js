a10855f05874e4cd73ee89b37283c85e3adbea80
bbudron <bbudron@gmail.com>
2018-12-28 16:48:08 -0500

    Displays response if user does not have any entries

--- client/src/components/days/DayList.js
+++ client/src/components/days/DayList.js
@@ -15,34 +15,51 @@ class DayList extends Component {
   }
 
   renderDays() {
-    return map(this.props.days, day => {
-      if(!Array.isArray(day)) {
-        return (
-          <div className="card darken-1 horizontal" key={day._id}>
-            <div className="card-stacked">
-              <div className="card-content">
-                <span className="cards-title">
-                  {(day.title.length > 16) ? (day.title.substring(0,16)+"...") : (day.title) }
-                </span>
-                <p className="cards-body">
-                  {(day.content.length > 30) ? (day.content.substring(0,30)+"...") : (day.content) }
-                </p>
-              </div>
-
-              <div className="card-actions">
-                <Link to={`/days/${day._id}`} className="card-action">
-                    <span className="card-link">Read Entry</span>
-                </Link>
-
-                <div className="card-action" onClick={() => this.deleteToday(day)}>
-                    <span className="card-link">Delete Entry</span>
-                </div>      
+    if(!(Object.keys(this.props.days).length === 0)) {
+      return map(this.props.days, day => {
+        if(!Array.isArray(day)) {
+          return (
+            <div className="card darken-1 horizontal" key={day._id}>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <span className="cards-title">
+                    {(day.title.length > 16) ? (day.title.substring(0,16)+"...") : (day.title) }
+                  </span>
+                  <p className="cards-body">
+                    {(day.content.length > 30) ? (day.content.substring(0,30)+"...") : (day.content) }
+                  </p>
+                </div>
+  
+                <div className="card-actions">
+                  <Link to={`/days/${day._id}`} className="card-action">
+                      <span className="card-link">Read Entry</span>
+                  </Link>
+  
+                  <div className="card-action" onClick={() => this.deleteToday(day)}>
+                      <span className="card-link">Delete Entry</span>
+                  </div>      
+                </div>
               </div>
             </div>
+          );
+        } else return null;
+      });
+    } else {
+      return (
+        <div className="card darken-1 horizontal">
+          <div className="card-stacked">
+            <div className="card-content">
+              <span className="cards-title">
+                Welcome
+              </span>
+              <p className="cards-body">
+                You have not created any diary entries, get started above!
+              </p>
+            </div>
           </div>
-        );
-      } else return null;
-    });
+        </div>
+      )
+    }
   }
 
   render() {
